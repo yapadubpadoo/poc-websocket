3808682d3deff88025da9487a81a18b6b81a2651
Nuttapon.Y <nuttapon@appsynth.net>
2022-01-07 18:02:35 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# poc-websocket
+
+Install
+
+```bash
+npm install
+```
+
+Run
+
+```bash
+./node_modules/.bin/ts-node src/index.ts
+```
+
+Access [http://localhost:3000](http://localhost:3000) and try to publish a message
+
+You can also subscribe to the message at `ws://localhost:3000`
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import * as Koa from 'koa';
+import * as serve from 'koa-static';
+import {Server} from 'socket.io';
+
+import * as path from 'path';
+import * as http from 'http';
+
+const app = new Koa();
+// https://github.com/koajs/koa/issues/1041#issuecomment-344318977
+const server = http.createServer(app.callback());
+const io = new Server(server, {
+  transports: ['websocket', 'polling'], // use WebSocket first, if available
+});
+
+const staticDirPath = path.join(__dirname, '..', 'static');
+app.use(serve(staticDirPath));
+
+// app.use(async ctx => {
+//   ctx.body = 'Hello World';
+// });
+
+io.on('connection', socket => {
+  console.log('[+] a user connected');
+  socket.on('disconnect', () => {
+    console.log('[-] user disconnected');
+  });
+  socket.on('message', msg => {
+    console.log('[>] ' + msg);
+    io.emit('message', msg); // also send to sender
+  });
+});
+
+// app.listen(3000);
+// https://github.com/koajs/koa/issues/1041#issuecomment-344318977
+server.listen(3000);
